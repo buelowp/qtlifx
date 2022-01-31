d850447cdbd3bb6f33b0f561d890e01fec54fe9b
Peter Buelow <goballstate@gmail.com>
2022-01-31 14:10:05 -0600

    Ditch initialize altogether
    * Honestly, just create the socket in the ctor, not later

--- src/lifxprotocol.cpp
+++ src/lifxprotocol.cpp
@@ -22,6 +22,9 @@
 LifxProtocol::LifxProtocol(QObject *parent) : QObject(parent)
 {
     m_socket = nullptr;
+    m_socket = new QUdpSocket();
+    m_socket->bind(LIFX_PORT);
+    connect(m_socket, &QUdpSocket::readyRead, this, &LifxProtocol::readDatagram);
 }
 
 LifxProtocol::~LifxProtocol()
@@ -63,21 +66,8 @@ bool LifxProtocol::newPacketAvailable()
     return m_socket->hasPendingDatagrams();
 }
 
-/**
- * If this is just starting up, then we need to create the new socket
- * but if we are trying again, say after a while, then we should
- * end the comms and restart them, but not recreate the socket
- */
 void LifxProtocol::initialize()
 {
-    if (!m_socket) {
-        m_socket = new QUdpSocket();
-        m_socket->bind(LIFX_PORT);
-        connect(m_socket, &QUdpSocket::readyRead, this, &LifxProtocol::readDatagram);
-    }
-    else {
-        m_socket->abort();
-    }
 }
 
 void LifxProtocol::getPowerForBulb(LifxBulb* bulb)
